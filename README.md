827ffe5089bea0a180604e924322f7f8159a9125
Faraz58 <faraz.montana@gmail.com>
2023-05-06 11:38:22 +0430

    Upload URL sortener on Github

---
+++ README.md
@@ -0,0 +1,28 @@
+# URL-shortener
+A simple URL shortener using python and django.
+
+With this simple tool you can shorten your long URL to a simple and short one.
+
+Tools
+---
+  - python
+  - django
+  - docker
+  - nginx
+  - html/css
+  
+Run Project
+---
+After cloning project, you can simply run this project by following instructions bellow.
+
+### 1- environment variables.
+create a `.env` file and set value for `SECRET_KEY`, `POSTGRES_NAME`, `POSTGRES_USER` and `POSTGRES_PASSWORD` keys.
+### 2- install docker and docker-compose.
+you can install docker from [here](https://docs.docker.com/engine/install/).
+
+### 3- docker commands.
+open a terminal and run command bellow.
+```
+docker-compose up
+```
+this command will run project on `localhost:8000` and you will be able to visit that by your favorite browser.
---
+++ shortner/views.py
@@ -0,0 +1,27 @@
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.views import View
+from .models import Link
+
+
+class index(View):
+    def get(self, request):
+        return render(request, 'index.htm')
+
+    def post(self, request):
+        url = request.POST.get("link")
+        link = Link.objects.create(url=url)
+        url = request.build_absolute_uri() + str(link.uuid)[-5:]
+        return render(
+            request, 'index.htm', context={'url': url}
+        )
+        
+
+class retrieve(View):
+    def get(self, request, pk):
+        try:
+            link = Link.objects.get(uuid__endswith=pk)
+            return redirect(to=link.url)
+        except Link.DoesNotExist:
+            return JsonResponse({'detail' :'This link does not exist in our database!'}, status=404)
+
